92d7b020a513c0405285b43025f033c26e817292
shourya <pihushourya100@gmail.com>
2026-09-14 14:00:59 +0530

    modified gym_env.py

--- scripts/gym_env.py
+++ scripts/gym_env.py
@@ -3,9 +3,20 @@ import gymnasium as gym
 from gymnasium import spaces
 import mujoco
 
+XML_PATH="/home/shourya/rl2/urdf/model.xml"
+
 class RotaryInvertedPendulumEnv(gym.Env):
-    def __init__(self):
+    metadata = {
+        "render_modes": ["human", None],
+        "render_fps": 50,
+    }
+    def __init__(self, render_mode=None):
         super().__init__()
+        self.render_mode = render_mode
+
+        self.model = mujoco.MjModel.from_xml_path(XML_PATH)
+        self.data = mujoco.MjData(self.model)
+
     def _get_obs(self): pass
     def _get_reward(self): pass
     def reset(self): pass

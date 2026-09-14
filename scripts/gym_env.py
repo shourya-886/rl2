a70b85821a7deef737baeeb996039cbe2c638f94
shourya <pihushourya100@gmail.com>
2026-09-14 13:27:26 +0530

    created gym_env.py

---
+++ scripts/gym_env.py
@@ -0,0 +1,14 @@
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
+import mujoco
+
+class RotaryInvertedPendulumEnv(gym.Env):
+    def __init__(self):
+        super().__init__()
+    def _get_obs(self): pass
+    def _get_reward(self): pass
+    def reset(self): pass
+    def step(self, action): pass
+    def render(self): pass
+    def close(self): pass
